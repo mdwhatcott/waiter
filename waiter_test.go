f24e7567724de44167dc9a42aa46d2d48249be5b
Michael Whatcott <mike@smarty.com>
2024-03-26 11:33:01 -0600

    Reinstate benchmarks.

--- waiter_test.go
+++ waiter_test.go
@@ -11,17 +11,17 @@ import (
 	"github.com/mdwhatcott/testing/should"
 )
 
-func wait() chan int {
+func wait(factory func() waiter.Waiter) chan int {
 	result := make(chan int, 100)
 	defer close(result)
 
-	sut := waiter.New()
+	sut := factory()
 	sut.Add(10)
 	for x := 0; x < 10; x++ {
 		go func(x int) { defer sut.Done(); result <- x }(x)
 	}
 
-	var helper sync.WaitGroup
+	helper := factory()
 	helper.Add(10)
 	for x := 0; x < 10; x++ {
 		go func(x int) { defer helper.Done(); sut.Wait(); result <- 10 + x }(x)
@@ -31,9 +31,33 @@ func wait() chan int {
 	return result
 }
 func Test(t *testing.T) {
-	result := Drain(wait())
+	result := Drain(wait(waiter.New))
 	should.So(t, len(result), should.Equal, 20)
 	should.So(t, set.Of(Take(10, result)...), should.Equal, set.Of(Range(0, 10)...))
 	should.So(t, set.Of(Drop(10, result)...), should.Equal, set.Of(Range(10, 20)...))
 	should.So(t, SortAscending(ByNumericValue[int], result), should.Equal, Range(0, 20))
 }
+
+func BenchmarkToy(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		wait(waiter.New)
+	}
+}
+func BenchmarkTool(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		wait(func() waiter.Waiter { return new(sync.WaitGroup) })
+	}
+}
+
+/*
+goos: darwin
+goarch: arm64
+pkg: waiter
+BenchmarkToy
+BenchmarkToy-10     	     984	   1473062 ns/op	    2244 B/op	      47 allocs/op
+BenchmarkTool
+BenchmarkTool-10    	  179563	      6375 ns/op	    2209 B/op	      43 allocs/op
+PASS
+*/
